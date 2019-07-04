5a861589c80fae7a72cff6046be3c81a00ce2d6c
pedro-hgm30 <pedro.hgm30@gmail.com>
2019-07-04 17:13:14 -0300

    added the pg gem

--- db/migrate/20190621163443_create_incomes.rb
+++ db/migrate/20190621163443_create_incomes.rb
@@ -5,7 +5,7 @@ class CreateIncomes < ActiveRecord::Migration[5.2]
       t.integer :year
       t.integer :month
       t.integer :day
-      t.text :description
+      t.text :description, default: "No description provided"
       # t.references :category, foreign_key: true
       t.references :account, foreign_key: true
       t.references :user, foreign_key: true
